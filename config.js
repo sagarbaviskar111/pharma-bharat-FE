0099fa9e14f1b4678534e1cac25c0d41c8690a01
sagar baviskar <68005331+sagarbaviskar111@users.noreply.github.com>
2025-06-25 17:33:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const CONFIG = {
     // BACKEND_URL: 'http://localhost:5000',
 
     // BACKEND_URL: 'http://api.pharmatalenthub.in',
-    BACKEND_URL: 'https://api.pharmatalenthub.in',
+    // BACKEND_URL: 'https://api.pharmatalenthub.in',
+       BACKEND_URL: 'https://pharma-bharat-be.onrender.com',
+
 
     // BACKEND_URL: 'http://167.99.228.98:5000',
 
